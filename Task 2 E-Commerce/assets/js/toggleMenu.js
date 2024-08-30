e609821eb81a7a8d88c8bbfd55dd720731388052
Vignesh01012003 <146185295+Vignesh01012003@users.noreply.github.com>
2024-08-31 01:32:26 +0530

    Add files via upload

---
+++ Task 2 E-Commerce/assets/js/toggleMenu.js
@@ -0,0 +1,16 @@
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px";
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
+
+let copyrightElem = document.getElementById('copyright-text');
+let currentYear = new Date().getFullYear()
+let fullText = `Copyright ${currentYear} - introidx`;
+copyrightElem.innerText = fullText;
